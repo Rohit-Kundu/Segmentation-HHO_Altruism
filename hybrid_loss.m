bebf9caa0716a9d88cab5763638c52666fecece2
Rohit-Kundu <80497999+Rohit-Kundu@users.noreply.github.com>
2021-06-28 12:18:55 +0530

    Update hybrid_loss.m

--- hybrid_loss.m
+++ hybrid_loss.m
@@ -1,3 +1,8 @@
+% Main paper:
+% Segmentation of Brain MRI using an Altruistic Harris Hawks' Optimization algorithm
+% Rajarshi Bandopadhyay, Rohit Kundu, Diego Oliva, Ram Sarkar
+% _____________________________________________________
+
 function [fit] = hybrid_loss(x,h)
     fit = 0.5*cross_entropy(x,h)+0.5*New_loss(x,h);
 end
