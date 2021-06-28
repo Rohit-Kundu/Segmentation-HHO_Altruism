5af84f0b1ccc3c773edae62dc28f31223b98a36b
Rohit-Kundu <80497999+Rohit-Kundu@users.noreply.github.com>
2021-06-28 12:15:15 +0530

    Update initialization.m

--- initialization.m
+++ initialization.m
@@ -1,10 +1,3 @@
-% Main paper:
-% Harris hawks optimization: Algorithm and applications
-% Ali Asghar Heidari, Seyedali Mirjalili, Hossam Faris, Ibrahim Aljarah, Majdi Mafarja, Huiling Chen
-% Future Generation Computer Systems, 
-% DOI: https://doi.org/10.1016/j.future.2019.02.028
-% _____________________________________________________
-
 function [X]=initialization(N,dim,up,down)
 
 if size(up,1)==1
@@ -15,5 +8,40 @@ if size(up,1)>1
         high=up(i);low=down(i);
         X(:,i)=rand(1,N).*(high-low)+low;
     end
-end
+    %X(1,:)=;random one
+    for i = 2:N
+        %sine
+        %X(i,:)=sin(pi*X(i-1,:));
+        
+        %singer
+        %X(i,:)=1.07*(7.868*(X(i-1,:)) -23.31*(X(i-1,:)*X(i-1,:))+ 28.75*(X(i-1,:)*X(i-1,:)*X(i-1,:))- 13.302875*(X(i-1,:)*X(i-1,:)*X(i-1,:)*X(i-1,:)));
+        
+        %sinusoidal
+        %X(i,:)= 2.3*(X(i-1,:)*X(i-1,:))* sin(pi*X(i-1));
+        
+        %chebyshev
+        %X(i,:)=cos(acosd(X(i-1,:)));
+        
+        %Tent
+        %if X(i-1,:)<0.7
+        %    X(i,:)= X(i-1,:)/0.7;
+        %end
+        %if X(i-1,:)>=0.7
+        %    X(i,:)= (10/3)*(1-X(i-1,:));
+        %end
+        
+        %Logistic
+        X(i,:)= 4*X(i-1,:)*(1-X(i-1,:));
+        
+        %Iterative
+        %X(i,:)=sin(0.7*pi/(X(i-1,:)));
+        
+        %Gauss
+        %if X(i-1,:)==0
+        %    X(i,:)= 1
+        %end
+        %if X(i-1,:)~=0
+        %   X(i,:)= 1/mod(X(i-1,:))
+        %end
+    end
 end
